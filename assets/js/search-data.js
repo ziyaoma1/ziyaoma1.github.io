15016efbb4e56756809cbd8c3820ed8a7c6b8212
ziyaoma1 <113070856+ziyaoma1@users.noreply.github.com>
2025-10-03 20:50:17 +0000

    Deploying to gh-pages from @ ziyaoma1/ziyaoma1.github.io@2425278b527c072af116536716e113d7ed856f5c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
