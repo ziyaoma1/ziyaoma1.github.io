54fd10523e8b7e5853a3199bfec4a9bd17008303
ziyaoma1 <113070856+ziyaoma1@users.noreply.github.com>
2025-10-04 02:38:54 +0000

    Deploying to gh-pages from @ ziyaoma1/ziyaoma1.github.io@7387d745da9b851e6a5b00a2340dd870fe48926a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
