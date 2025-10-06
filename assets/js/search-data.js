941d4fc193cddc51a9b5ddab47d5d814a6d67566
ziyaoma1 <113070856+ziyaoma1@users.noreply.github.com>
2025-10-06 17:48:50 +0000

    Deploying to gh-pages from @ ziyaoma1/ziyaoma1.github.io@112674d59f8d43c26ca41ddf149bedbfced3b8e7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -424,11 +424,6 @@ ninja.data = [{
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project.html";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project.html";
             },},{
         id: 'social-email',
         title: 'email',
